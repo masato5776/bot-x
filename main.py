4b8d88f11bb65816476f25784257ecd35810a6c5
masato5776 <masato.hayashi.57@gmail.com>
2024-01-08 17:08:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import tweepy
+from datetime import datetime
 
 def main(request):
   bt = os.environ.get('bearer_token')
@@ -9,8 +10,13 @@ def main(request):
   cs = os.environ.get('consumer_secret')
   at = os.environ.get('access_token')
   ats = os.environ.get('access_token_secret')
+
+  end_day = datetime(2020, 7, 24)
+  today = datetime.now()	
+  delta = end_day - today	
+  days = delta.days + 1	
   
   api = tweepy.Client(bearer_token=bt,consumer_key=ck,consumer_secret=cs,access_token=at,access_token_secret=ats)
-  tweet = api.create_tweet(text="Hello World Local.")
+  tweet = api.create_tweet(text=f"あと{days}日")
 
   return f"OK {bt},{ck},{cs},{at},{ats}"
